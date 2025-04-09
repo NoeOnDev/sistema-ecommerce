411341667598721f36ef8eb80a5fef3146d207a1
NoeOnDev <alxg5516@gmail.com>
2025-04-09 14:43:20 -0600

    feat: Añadir seeder para usuario cliente en la base de datos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
             TagSeeder::class,
             ProductSeeder::class,
             AdminUserSeeder::class,
+            ClienteUserSeeder::class,
         ]);
     }
 }
